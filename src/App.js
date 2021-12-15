a08b983460a76c57b513186fe8fad21dd4aa1610
Sandesh Shetty <sandesh.s@pacewisdom.com>
2021-12-15 18:37:10 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Input from './Components/Input/Input';
+import Logo from './Components/Logo/Logo';
+// import TodoList from './Components/TodoList/TodoList';
+
+function App() {
+  return (
+    <div>
+      <Logo />
+      <Input />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Logo.scss'
+
+const Logo =() => {
+    return (
+        <div className="logo">
+            <h2><span className="title">Todo</span>List</h2>
+        </div>
+    )
+}
+
+export default Logo;
