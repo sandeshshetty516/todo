07ba1d4f9b65a86a406130ff28df14449b0c2744
Sandesh Shetty <sandesh.s@pacewisdom.com>
2021-12-16 13:11:31 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.scss';
 import Input from './Components/Input/Input';
 import Logo from './Components/Logo/Logo';
-// import TodoList from './Components/TodoList/TodoList';
 
 function App() {
   return (
